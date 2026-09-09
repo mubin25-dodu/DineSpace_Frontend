4ecdef91a711074c907ce4f0c01f0a20c85a580f
mubin25-dodu <mubin9516@gmail.com>
2026-09-09 23:51:51 +0600

    1

--- src/lib/interfaces/order.ts
+++ src/lib/interfaces/order.ts
@@ -3,12 +3,13 @@ import { ReactNode } from "react";
 import { Files } from "./file";
 
 export interface OrderItem {
-    id: string;
-    orderId: string;
-    itemId: string;
+    id?: string;
+    orderId?: string;
+    itemId?: string;
     menu: MenuItem;
     quantity: number;
-    price: string;
+    price: number;
+    resturantId?: string;
 }
 
 export interface MenuItem {
